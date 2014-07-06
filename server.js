8993cb2de8dbcbb320fdeb8f72d62fe3ec30ee08
Frank <pussinboots666@googlemail.com>
2014-07-06 18:11:55 +0200

    - use exec sync instead of exec

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var git  = require('gift');
 
 var fs = require('fs');
 var url = require('url');
-var sys = require('sys')
-var exec = require('child_process').exec;
+//var sys = require('sys')
+//var exec = require('child_process').exec;
+var execSync = require('exec-sync');
 
 var port = Number(process.env.PORT || 9000);
 var server = connect()
@@ -18,24 +19,21 @@ server.use(function (req, res, next) {
 		next();
 	} else {
 		var repo = url.parse(req.url, true).query.repo;
-		exec("rm -rfv " + repo,{cwd: '/tmp/'}, function (error, stdout, stderr) { 
-		  			sys.puts(stdout);
-		  		});
-		
-			console.log('try to clone git@github.com:' + repo);
-			git.clone("https://github.com/" + repo, "/tmp/"+repo, function(err, _repo) {
-		  		console.log('repo ' + _repo);
-		  		console.log('err ' + err);
-		  		exec("softcover build:pdf",{cwd: '/tmp/'+repo}, function (error, stdout, stderr) { 
-		  			res.write(stdout);
-		  			res.end();
-		  			/*sys.puts(stdout);
-		  			var fileStream = fs.createReadStream('/tmp/'+repo +'/ebooks/example.pdf');
-					res.writeHead(200, {'Content-Type': 'application/pdf', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
-	        		fileStream.pipe(res);*/
-		  		});
-		  	})
-		
+		var result = execSync("rm -rfv /tmp/" + repo)
+		console.log(result.stdout);
+		console.log('try to clone git@github.com:' + repo);
+		git.clone("https://github.com/" + repo, "/tmp/"+repo, function(err, _repo) {
+	  		console.log('repo ' + _repo);
+	  		console.log('err ' + err);
+	  		var proc = execSync('cd /tmp/' + repo +' && softcover build:pdf',{cwd: '/tmp/'+repo}); 
+	  			/*sys.puts(stdout);
+	  			var fileStream = fs.createReadStream('/tmp/'+repo +'/ebooks/example.pdf');
+				res.writeHead(200, {'Content-Type': 'application/pdf', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
+        		fileStream.pipe(res);*/
+
+		    res.write(proc.stdout)
+	        res.end();
+	  	})	
 	}
 });
 
