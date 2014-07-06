7d350f7775a7cd332aff9eaddfec9ae289cb5dc0
Frank <pussinboots666@googlemail.com>
2014-07-06 19:52:34 +0200

    working version for aws

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var git  = require('gift');
 
 var fs = require('fs');
 var url = require('url');
-//var sys = require('sys')
-//var exec = require('child_process').exec;
-var execSync = require('exec-sync');
+var sys = require('sys')
+var exec = require('child_process').exec;
+//var execSync = require('exec-sync');
 
 var port = Number(process.env.PORT || 9000);
 var server = connect()
@@ -19,22 +19,39 @@ server.use(function (req, res, next) {
 		next();
 	} else {
 		var repo = url.parse(req.url, true).query.repo;
-		var result = execSync("rm -rfv tmp/" + repo);
-		console.log(result.stdout);
-		console.log('try to clone git@github.com:' + repo);
-		git.clone("https://github.com/" + repo, "tmp/"+repo, function(err, _repo) {
-	  		console.log('repo ' + _repo);
-	  		console.log('err ' + err);
-	  		var proc = execSync('cd tmp/' + repo +' && softcover build:pdf',{cwd: '/tmp/'+repo}); 
-	  			/*sys.puts(stdout);
-	  			var fileStream = fs.createReadStream('/tmp/'+repo +'/ebooks/example.pdf');
-				res.writeHead(200, {'Content-Type': 'application/pdf', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
-        		fileStream.pipe(res);*/
-        	res.writeHead(200, {'Content-Type': 'test/plain', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
-		    res.write(proc.stdout);
-		    console.log(proc.stdout);
-	        res.end();
-	  	})	
+		console.log("checkl if exists /tmp/" +repo);
+		if (fs.existsSync("/tmp/" +repo)) {
+			console.log('try to sync git@github.com:' + repo);
+			var repository = git("/tmp/"+repo);
+	    	repository.checkout('master', function(err, _repo) {
+		  		console.log('synced repo ' + _repo);
+		  		console.log('err ' + err);
+		  		exec("softcover build:pdf",{cwd: '/tmp/'+repo}, function (error, stdout, stderr) { 
+		  			/*res.writeHead(200,{"Content Type": "text/plain"});
+		  			res.write(stdout);
+		  			res.end();*/
+		  			sys.puts(stdout);
+		  			var fileStream = fs.createReadStream('/tmp/'+repo +'/ebooks/example.pdf');
+					res.writeHead(200, {'Content-Type': 'application/pdf', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
+	        		fileStream.pipe(res);
+		  		});
+		  	})
+		} else {
+			console.log('try to clone git@github.com:' + repo);
+			git.clone("https://github.com/" + repo, "/tmp/"+repo, function(err, _repo) {
+		  		console.log('repo ' + _repo);
+		  		console.log('err ' + err);
+		  		exec("softcover build:pdf",{cwd: '/tmp/'+repo}, function (error, stdout, stderr) { 
+		  			/*res.writeHead(200,{"Content Type": "text/plain"});
+		  			res.write(stdout);
+		  			res.end();*/
+		  			sys.puts(stdout);
+		  			var fileStream = fs.createReadStream('/tmp/'+repo +'/ebooks/example.pdf');
+					res.writeHead(200, {'Content-Type': 'application/pdf', "Cache-Control:" : "no-cache, no-store, must-revalidate" });
+	        		fileStream.pipe(res);
+		  		});
+		  	})
+	  	}	
 	}
 });
 
